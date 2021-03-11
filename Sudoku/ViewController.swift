c28171d2991e61056d875d93fd065f01b98ba97b
sainkr <raebadnap@naver.com>
2021-03-11 23:29:19 +0900

    Initial Commit

---
+++ Sudoku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sudoku
+//
+//  Created by 홍승아 on 2021/03/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
